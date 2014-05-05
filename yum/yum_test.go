2a82649667e678c702b7a3ed5e7961c3ea1b2a06
Sebastien Binet <binet@cern.ch>
2014-05-05 13:18:17 +0200

    test: enable TestLocaConfig for sqlite

--- yum/yum_test.go
+++ yum/yum_test.go
@@ -355,13 +355,13 @@ func TestLoadConfig(t *testing.T) {
 			siteroot: "testdata/testconfig-xml",
 			backends: []string{"RepositoryXMLBackend"},
 		},
-		// {
-		// 	siteroot: "testdata/testconfig-sqlite",
-		// 	backends: []string{
-		// 		"RepositorySQLiteBackend",
-		// 		"RepositoryXMLBackend",
-		// 	},
-		// },
+		{
+			siteroot: "testdata/testconfig-sqlite",
+			backends: []string{
+				"RepositorySQLiteBackend",
+				"RepositoryXMLBackend",
+			},
+		},
 	} {
 		siteroot := table.siteroot
 		checkForUpdates := false

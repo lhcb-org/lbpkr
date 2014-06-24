f51b53b4e66d6fc7347d3e6ec42607133e9c7069
Sebastien Binet <binet@cern.ch>
2014-06-24 10:49:37 +0200

    cmd-remove: first stab

---
+++ cmd_remove.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/gonuts/commander"
+	"github.com/gonuts/flag"
+)
+
+func lbpkr_make_cmd_remove() *commander.Command {
+	cmd := &commander.Command{
+		Run:       lbpkr_run_cmd_remove,
+		UsageLine: "remove [options] <rpmname> [<version> [<release>]]",
+		Short:     "remove a RPM from the yum repository",
+		Long: `
+remove removes a RPM from the yum repository.
+
+ex:
+ $ lbpkr remove LHCb
+`,
+		Flag: *flag.NewFlagSet("lbpkr-remove", flag.ExitOnError),
+	}
+	cmd.Flag.Bool("v", false, "enable verbose mode")
+	cmd.Flag.String("type", "lhcb", "config type (lhcb|atlas)")
+	cmd.Flag.Bool("force", false, "force removal of RPM")
+	return cmd
+}
+
+func lbpkr_run_cmd_remove(cmd *commander.Command, args []string) error {
+	var err error
+
+	cfgtype := cmd.Flag.Lookup("type").Value.Get().(string)
+	debug := cmd.Flag.Lookup("v").Value.Get().(bool)
+	force := cmd.Flag.Lookup("force").Value.Get().(bool)
+
+	rpmname := ""
+	version := ""
+	release := ""
+	switch len(args) {
+	case 0:
+		cmd.Usage()
+		return fmt.Errorf("lbpkr: invalid number of arguments (got=%d)", len(args))
+	case 1:
+		rpmname = args[0]
+	case 2:
+		rpmname = args[0]
+		version = args[1]
+	case 3:
+		rpmname = args[0]
+		version = args[1]
+		release = args[2]
+	default:
+		return fmt.Errorf("lbpkr: invalid number of arguments. expected n=1|2|3. got=%d (%v)",
+			len(args),
+			args,
+		)
+	}
+
+	re := regexp.MustCompile(`(.*)-([\d\.]+)-(\d)$`).FindAllStringSubmatch(rpmname, -1)
+	if len(re) == 1 {
+		m := re[0]
+		switch len(m) {
+		case 2:
+			rpmname = m[1]
+		case 3:
+			rpmname = m[1]
+			version = m[2]
+		case 4:
+			rpmname = m[1]
+			version = m[2]
+			release = m[3]
+		}
+	}
+
+	cfg := NewConfig(cfgtype)
+	ctx, err := New(cfg, debug)
+	if err != nil {
+		return err
+	}
+	defer ctx.Close()
+
+	ctx.msg.Infof("removing RPM %s %s %s\n", rpmname, version, release)
+
+	err = ctx.RemoveRPM(rpmname, version, release, force)
+	return err
+}
